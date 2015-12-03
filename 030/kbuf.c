d564e1b2e26ee8b9345936eafda0c078f0439ebc
Nasturtium <phonya@yandex.ru>
2015-12-03 07:31:47 -0800

    my work

--- 030/kbuf.c
+++ 030/kbuf.c
@@ -2,10 +2,14 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/errno.h>
+#include <linux/cdev.h>
 
 #define MY_NAME "kbuf"
 
 static const int major = 800;
+static const int minor = 0;
+
+struct cdev *my_cdev;
 
 int chardev_open(struct inode *inode,struct file *file)
 {
@@ -35,9 +39,17 @@ static const struct file_operations chardev_fops =
 						};
 static int init_func(void)
 {
+	dev_t first_node = MKDEV(major,minor);
+
 	printk(KERN_INFO "init module ! ! ! \n");
+
+	register_chrdev_region(first_node,1,"kbuf");
+	my_cdev = cdev_alloc();
+
 	register_chrdev(major,MY_NAME,&chardev_fops);
-	
+        cdev_init(my_cdev,&chardev_fops);
+        cdev_add(my_cdev,first_node,minor);
+
 	return 0;
 }
 
